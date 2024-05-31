f955cb320d88f87f699d8775cb0fd7a77ce30933
BAD BOY <ddt-sf@yandex.ru>
2024-05-31 10:02:34 +0300

    # 1. Форматирование строки: Дана строка с именем и фамилией человека, разделёнными пробелом. # Написать программу, которая принимает эту строку и печатает ее в другом формате: фамилия, инициалы. Например, # "Иван Сидоров" преобразуется в "Сидоров И."
    # 2. Парсинг даты: Дана строка, содержащая дату в формате "ДД-ММ-ГГГГ".
    # Написать программу, которая принимает эту строку и печатает в другом формате: (день, месяц, год).

---
+++ ДЗ работа со строками.py
@@ -0,0 +1,24 @@
+string1 = "Иван Сидоров"
+first_letter= string1[0]
+a = first_letter
+slice = string1[5:12]
+b= slice
+print(a,b)
+
+
+#date=input('Enter a date(dd/mm/yyyy)')
+#replace=date.replace('/',' ')
+#convert=replace.split()
+#day=convert[1:2]
+#year=convert[2:4]
+#print("день",day)
+
+date_string = input('Enter a date using the (dd.mm.yyyy) format: ')
+date_list = date_string.split('.')
+month = date_list[1]
+day = date_list[0]
+year_ = date_list[2]
+print(day, month, ',', year_ )
+input('Press enter to end: ')
+
+
