3b1c5d481e149820761d976373cf0ab75bcff0d0
BAD BOY <ddt-sf@yandex.ru>
2024-06-11 20:11:15 +0300

    11.06 классная работа задача на минимальный элемент в списке

---
+++ 11.06 Задача 2.py
@@ -0,0 +1,16 @@
+data = []
+n = int(input())
+for i in range(n):
+    new_elem = int(input())
+    data.append(new_elem)
+print(len([elem for elem in data if elem > 0]))
+
+print(data)
+minimum = 1_000_000
+for elem in data:
+    if elem > 0 and (minimum is None or elem < minimum):
+        minimum = elem
+
+print(minimum)
+
+
